0770cbb986ab16779cf27a159181ab75f3c86167
AJ-Romera <ajromeraalfonso@gmail.com>
2021-03-10 12:20:13 +0100

    CHNG: Estiliza la Cotización con Styled Components

--- src/componentes/Cotizacion.js
+++ src/componentes/Cotizacion.js
@@ -1,12 +1,42 @@
 import React from 'react';
 import styled from '@emotion/styled';
 
+const ResultadoDiv = styled.div`
+    color: #fff;
+    font-family: 'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande',
+        'Lucida Sans', Arial, sans-serif;
+`;
+
+const Contenedor = styled.div`
+    display: grid;
+    grid-template-columns: 60% 1fr;
+    column-gap: 2rem;
+`;
+
+const Precio = styled.p`
+    font-size: 30px;
+    span {
+        font-weight: bold;
+    }
+`;
+
+const Info = styled.p`
+    font-size: 18px;
+    span {
+        font-weight: bold;
+    }
+`;
+
 const GreenSpan = styled.span`
     color: green;
 `;
 
 const RedSpan = styled.span`
     color: red;
+`;
+
+const CriptoImg = styled.img`
+    width: 70px;
 `;
 
 function Cotizacion({ resultado }) {
@@ -19,31 +49,33 @@ function Cotizacion({ resultado }) {
     const criptoImgUrl = `https://www.cryptocompare.com${resultado.IMAGEURL}`;
 
     return (
-        <div>
-            <p>
+        <ResultadoDiv>
+            <Precio>
                 El precio es: <span>{resultado.PRICE}</span>
-            </p>
-            <p>
-                Variación Últimas 24h:{' '}
-                {resultado.CHANGEPCT24HOUR >= 0 ? (
-                    <GreenSpan>{resultado.CHANGEPCT24HOUR}%</GreenSpan>
-                ) : (
-                    <RedSpan>{resultado.CHANGEPCT24HOUR}%</RedSpan>
-                )}
-            </p>
-            <img src={criptoImgUrl} alt='Imagen criptomoneda' />
-            <p>
+            </Precio>
+            <Contenedor>
+                <Info>
+                    Variación Últimas 24h:{' '}
+                    {resultado.CHANGEPCT24HOUR >= 0 ? (
+                        <GreenSpan>{resultado.CHANGEPCT24HOUR}%</GreenSpan>
+                    ) : (
+                        <RedSpan>{resultado.CHANGEPCT24HOUR}%</RedSpan>
+                    )}
+                </Info>
+                <CriptoImg src={criptoImgUrl} alt='Imagen criptomoneda' />
+            </Contenedor>
+            <Info>
                 Precio más bajo en las últimas 24h:{' '}
                 <span>{resultado.HIGH24HOUR}</span>
-            </p>
-            <p>
+            </Info>
+            <Info>
                 Precio más bajo en las últimas 24h:{' '}
                 <span>{resultado.LOW24HOUR}</span>
-            </p>
-            <p>
+            </Info>
+            <Info>
                 Volumen últimas 24h: <span>{resultado.VOLUME24HOURTO}</span>
-            </p>
-        </div>
+            </Info>
+        </ResultadoDiv>
     );
 }
 
